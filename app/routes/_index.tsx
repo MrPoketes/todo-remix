04ee79960204631c09a6360e65f3a482d2b441b9
MrPoketes <armintas.zadeika@gmail.com>
2023-11-22 19:29:57 +0200

    first commit

---
+++ app/routes/_index.tsx
@@ -0,0 +1,12 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return <div className="bg-red-500">x</div>;
+}
